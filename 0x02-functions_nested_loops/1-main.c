1d2451c9010f2fd21451203c3e730232de7d762f
HeyRoxie <dr.roxiecrux@gmail.com>
2022-12-21 03:27:17 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - check the code
  * Return: Always 0.
@@ -9,6 +7,6 @@
 
 int main(void)
 {
-	    print_alphabet();
-	    return (0);
+	print_alphabet();
+	return (0);
 }
